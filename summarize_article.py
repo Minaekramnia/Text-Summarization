f640abbbaa4c53baaac7e5fb75809972e9a9f301
russell-pollari <pollarir@mgail.com>
2020-05-20 15:50:53 -0400

    Add test article and function to parse into sentences

---
+++ summarize_article.py
@@ -0,0 +1,13 @@
+from nltk.tokenize import sent_tokenize
+
+
+def get_setences_from_file(filename):
+    with open(filename) as f:
+        text = f.read()
+
+    return sent_tokenize(text)
+
+
+if __name__ == '__main__':
+    sentences = get_setences_from_file('test-article.txt')
+    print(sentences)
